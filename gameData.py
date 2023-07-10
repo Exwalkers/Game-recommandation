eea8fb1079ea6aef4a564fffd3cecb109e8ac52b
[Paul Brittain] <[cursives@hotmail.co.uk]>
2023-07-10 16:23:19 +0200

    Added a greet function
    Added greet function, plus started to work on data structure

---
+++ gameData.py
@@ -0,0 +1,7 @@
+genre = ['Action', 'Survial Horror', 'Adventure', 'Puzzle', 'Role-playing', 'Simulation', 'Strategy', 'Sports', 'MMO', 'Sandbox']
+
+action = ['Platform', 'Shooter', 'Fighting', 'Stealth', 'Survival', 'Rhythm', 'Battle Royale']
+
+role_playing = ['Action RPG', 'MMORPG', 'Roguelikes', 'Tactical RPG', 'Sandbox RPG']
+
+game_data = [['Type', "Game name", "age"]]
